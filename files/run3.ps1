0912165517cb421b7533cd062279946638b09a2b
Mostafa Dadkhah <80312750+M-dadkhah@users.noreply.github.com>
2022-04-25 16:18:19 -0400

    Delete files directory

--- files/run3.ps1
+++
@@ -1,9 +0,0 @@
-cd 'E:\mostafa\PhD\COMP579\project\codes\COMP579-Project-Template'
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
-python '.\train_agent.py' --agentFile .agent_td3
