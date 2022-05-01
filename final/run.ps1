95182f85a9a165f53d09f0e03fa53aabf3e4bbdd
Mostafa Dadkhah <80312750+M-dadkhah@users.noreply.github.com>
2022-04-30 23:22:20 -0400

    Delete final directory

--- final/run.ps1
+++
@@ -1,9 +0,0 @@
-﻿cd 'E:\mostafa\PhD\COMP579\project\codes\final'
-# for ($i = 1 ; $i -le 5 ; $i++) {
-    for ($seed = 0 ; $seed -le 3 ; $seed++) {
-        python '.\train_agent.py' --algo td3 --seed $seed
-        # python '.\train_agent.py' --algo ddpg --seed $seed
-    }
-# }
-
-# python '.\anim.py' --algo td3Anim --seed 0
